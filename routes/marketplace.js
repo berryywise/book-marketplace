6916ebb007e353377485ebd2ad592e43e2d27995
berry <100073317+berryywise@users.noreply.github.com>
2024-05-15 16:09:54 +0200

    Removed sidebar content, added product page content

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -66,8 +66,9 @@ router.get("/", ensureAuthenticated, async (req, res) => {
 router.get("/book/:bookId", ensureAuthenticated, async (req, res) => {
 
     const bookId = req.params.bookId
+    const filteredBook = await Product.findOne({_id: bookId, in_review: false, deleted: false, declined: false,})
 
-    res.render("product-page", {user: req.user})
+    res.render("product-page", {user: req.user, filteredBook})
 
 })
 
